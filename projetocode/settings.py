f417e78648843809b95dd775fb921796575e908b
Filippe <revigat@gmail.com>
2016-07-06 23:54:21 -0300

    Commit com BUG

--- projetocode/settings.py
+++ projetocode/settings.py
@@ -124,3 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
