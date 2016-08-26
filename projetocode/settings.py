4c6643f4e9a4e7f91f44a9e8400b1f5345791373
Filippe <revigat@gmail.com>
2016-08-26 19:49:24 -0300

    Mensagem de sucesso

--- projetocode/settings.py
+++ projetocode/settings.py
@@ -131,6 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATICFILES_DIRS = (
+    os.path.join(STATIC_ROOT, 'static'),
+)
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
