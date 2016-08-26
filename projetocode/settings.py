655a63c21b53b8559acb1d1ac44e9d6a75fa836d
Filippe <revigat@gmail.com>
2016-08-26 20:16:15 -0300

    Mensagem de sucesso

--- projetocode/settings.py
+++ projetocode/settings.py
@@ -131,9 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT, 'static'),
-)
 
 
 # Simplified static file serving.
